b71d81d8492ee980f9337b3ff846e466732dd50e
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-12-23 12:36:54 -0300

    Exercises 3.14 a 3.18

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const app = express()
 app.use(cors())
 app.use(express.static('build'))
 
+
+
+
 ///////////////////////////////////////////////////////////////////////////////
 // setup the logger
 
@@ -48,10 +51,18 @@ app.get('/api/persons', (req, res) => {
 })
 
 /////////////////////////////////////////////////
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(persons => {
-        res.json(persons)
-    })
+app.get('/api/persons/:id', (req, res,next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            (person)
+                ?res.json(person)
+                :res.json({code:'Not Found'}).status(404).end() 
+        })
+    .catch(error => {
+        console.log("WAT IS THIS",error)
+        next(error)
+      })
+
 })
 
 
@@ -70,27 +81,76 @@ app.post('/api/persons', (req, res) => {
     query.count((err, count) => {
         if (err || count > 0) {
             return res.status(404).json({ error: `name must be unique` })
-        }else{
+        } else {
             const out = Person.create(newPerson);
             res.json(out).status(200);
         }
     })
-
-    
 })
+
+/////////////////////////////////////////////////
+app.put('/api/persons', (req, res) => {
+
+    const newPerson = req.body;
+
+    if (!newPerson.name) { return res.status(404).json({ error: `The 'name' field is required` }) }
+
+    if (newPerson.name.trim().length === 0) {
+        return res.status(404).json({ error: `The 'name' field must be not empty` })
+    }
+
+    const query = { name: newPerson.name };
+    Person.findOneAndUpdate( 
+        query,
+        { $set:{ name: newPerson.name, number: newPerson.number }},
+        (err,docs) => { 
+            if (err)  { 
+                console.log(err) 
+                return res.status(404).json({ error: `Error updating Docs`, err })            
+            }
+            else { 
+                console.log("Updated Docs : ", newPerson);
+                return res.json(newPerson).status(200);
+            } 
+        }
+        );
+    // query.count((err, count) => {
+    //     if (count === 1) {
+    //         const out = Person.updateOne( { name: newPerson.name }, {number:number }   )
+    //         res.json(out).status(200);
+    //     } else {
+    //         return res.status(404).json({ error: `Not Found name`, err })            
+    //     }
+})
+
 
 
 /////////////////////////////////////////////////
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    // para borrar copia todo menos el que llega por id
-    const newPersons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+
+    Person.findByIdAndRemove(req.params.id)
+        .then(result => { res.status(204).end() })
+        .catch(error => next(error))
+
 })
 
 
+///////////////////////////////////////////////////////////////////////// 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
+////////////////////////////////////////////////////////////////////////
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 
 //////////////////MAIN//////////////////
